b7fc493e720f0f8aba10f322704a1c571e04f3c0
Adam <xdocad01@gjk.cz>
2022-03-24 08:44:46 +0100

    working registration app

--- register/views.py
+++ register/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def register_view(response):
-    return render()
+    form = UserCreationForm()
+    return render(response, "login.html", {"form":form})
