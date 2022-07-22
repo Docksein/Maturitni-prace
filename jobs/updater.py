c804f763c99a66685a2cd56ca8e87fb08a118587
Adam <xdocad01@gjk.cz>
2022-07-22 09:23:37 +0200

    Updated html for unrated foods

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,5 +4,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(job, 'cron', day_of_week = 'mon-fri', hour = 11, minute = 30)
+    scheduler.add_job(job, 'cron', day_of_week = 'mon-fri', hour = 11, minute = 00)
     scheduler.start()
