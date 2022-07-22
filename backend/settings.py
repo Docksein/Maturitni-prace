997f8dc2e301ee2393678f7ab74637e48324c36f
Adam <xdocad01@gjk.cz>
2022-07-22 08:42:37 +0200

    added default picture for foods

--- backend/settings.py
+++ backend/settings.py
@@ -123,7 +123,7 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
